8cfb11a437ca6c9f7bb56832bb974afd34db5d9c
EDWIN FIDENCIO OSORIO JUAQUIN <202114044@uns.edu.pe>
2025-11-03 17:04:09 -0500

    mejora noticia nacional e internacional

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -1,11 +1,8 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "REAL MADRID GOLEA AL BARCELONA",
+  "autor" => "Edwin Osorio",
+  "resumen" => "El Club de Fútbol Real Madrid le da una paliza histórica de 9 a 0 al Fútbol Club Barcelona en la final de la Champions League 2026.",
+  "imagen" => "https://upload.wikimedia.org/wikipedia/commons/9/98/Real_Madrid_CF.svg" // ejemplo de imagen pública
 ];
 ?>
--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -1,11 +1,8 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "UNIVERSIDADES PERUANAS SE DIGITALIZAN CON PLATAFORMAS DE FIRMA ELECTRÓNICA",
+  "autor" => "Edwin Osorio Juaquin",
+  "resumen" => "El Ministerio de Educación impulsa un proyecto nacional de digitalización académica que permitirá a las universidades del Perú implementar sistemas de firma electrónica para actas, certificados y documentos oficiales. La iniciativa busca reducir el uso de papel y mejorar la transparencia administrativa. La Universidad Nacional del Santa destaca como pionera en este proceso, integrando tecnología de validación digital y almacenamiento seguro en la nube.",
+  "imagen" => "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Peru.svg/320px-Flag_of_Peru.svg.png"
 ];
 ?>
