2f677f57672ec6cad7852895a1dfa494e8762adf
EDWIN FIDENCIO OSORIO JUAQUIN <202114044@uns.edu.pe>
2025-11-03 17:15:21 -0500

    update portada

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $portada = [
 "titulo" => "OSORIO JUAQUIN EDWIN",
 "autor" => "eosorio",
-"resumen" => "Ingeniero de Sistemas 2025",
+"resumen" => "Las noticias mas importantes del 2025",
 ];
 ?>
